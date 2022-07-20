69aff3339f0819f720b28be3d53f3bec410119fb
NikitaStepanyuk <st.nikita6666@gmail.com>
2022-07-20 18:23:14 +0300

    Commit Message

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -2,6 +2,9 @@ package koschei.models;
 
 public class Needle7 {
 
+    public Needle7(Deth8 deth) {
+    }
+
     @Override
     public String toString() {
         return ", смерть Кощея на игле :( " + "";
